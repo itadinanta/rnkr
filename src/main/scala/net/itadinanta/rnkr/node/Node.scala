d6b168b77fbff9acb94cd96623d27ac15ab8999b
Nicola Orru <nigu.orru@gmail.com>
2014-05-08 01:35:27 +0100

    Working node, still working my head around the Scala type system

--- src/main/scala/net/itadinanta/rnkr/node/Node.scala
+++ src/main/scala/net/itadinanta/rnkr/node/Node.scala
@@ -1,5 +1,7 @@
 package net.itadinanta.rnkr.node
 
+import scala.collection.mutable.ListBuffer
+
 trait Ordering[T] {
 	def lt(a: T, b: T): Boolean
 	final def eq(a: T, b: T) = a == b
@@ -8,50 +10,55 @@ trait Ordering[T] {
 	final def ge(a: T, b: T) = !lt(a, b)
 }
 
-trait Node {
-	def size: Int
+trait Node[V] {
+	val size: Int
+	def values: Seq[V]
 }
 
-trait ChildNode extends Node
+trait ChildNode[V] extends Node[V]
 
-trait WithChildren[K, T <: ChildNode] {
+trait WithChildren[K, V] {
 	val keys: Seq[K]
-	val children: Seq[T]
+	val children: Seq[ChildNode[V]]
+	def values: Seq[V]
 	def indexOfKey(key: K) = keys.indexOf(key)
-	def indexOfChild(child: T) = children.indexOf(child)
+	def indexOfChild(child: ChildNode[V]) = children.indexOf(child)
 	def childOfKey(key: K) = children(indexOfKey(key))
 	def childAt(index: Int) = children(index)
 	def keyAt(index: Int) = keys(index)
 }
 
-trait RootNode[K, T <: ChildNode] extends Node with WithChildren[K, T]
+trait RootNode[K, V] extends Node[V] with WithChildren[K, V]
 
-trait InternalNode[K, T <: ChildNode] extends ChildNode with WithChildren[K, T]
+trait InternalNode[K, V] extends ChildNode[V] with WithChildren[K, V]
 
-trait LeafNode[K, V] extends ChildNode with WithChildren[K, Record[V]] {
+trait LeafNode[K, V] extends ChildNode[V] with WithChildren[K, V] {
 	val next: LeafNode[K, V]
 	val prev: LeafNode[K, V]
 }
 
-trait Record[V] extends ChildNode {
-	override def size = 1
-	def data: V
+trait Record[V] extends ChildNode[V] {
+	override val size = 1
+	val data: V
+	override def values: Seq[V] = Seq(data)
 }
 
 trait RecordBuilder[K, V] {
-	def newNode(key: K, data: V): WithChildren[K, Record[V]]
+	def newNode(key: K, data: V): WithChildren[K, V]
 	def newRecord(data: V): Record[V]
 }
 
-trait ChildrenBuilder[K, T <: ChildNode] {
-	def newNode(keys: Seq[K], children: Seq[T]): WithChildren[K, T]
-	def emptyNode(): WithChildren[K, T]
-	def insert(node: WithChildren[K, T], key: K, child: T): NodePointer[K, T]
-	def delete(node: WithChildren[K, T], key: K): NodePointer[K, T]
-	def update(node: WithChildren[K, T], key: K, child: T): NodePointer[K, T]
+trait ChildrenBuilder[K, V] {
+	def newNode(keys: Seq[K], children: Seq[ChildNode[V]]): WithChildren[K, V]
+	def emptyNode(): WithChildren[K, V]
+	def insert(node: WithChildren[K, V], key: K, child: ChildNode[V]): NodeRef[K, V]
+	def delete(node: WithChildren[K, V], key: K): NodeRef[K, V]
+	def update(node: WithChildren[K, V], key: K, child: ChildNode[V]): NodeRef[K, V]
 }
 
-class NodePointer[K, V](val node: Node, val key: K, val value: V, val index: Int)
+class NodeRef[K, V](val node: WithChildren[K, V], val key: K, val child: ChildNode[V], val index: Int)
+
+class NodePointer[K, V](val node: WithChildren[K, V], val key: K, val value: V, val index: Int)
 
 object IntAscending extends Ordering[Int] {
 	override def lt(a: Int, b: Int): Boolean = a < b
@@ -77,55 +84,60 @@ object StringCIDescending extends Ordering[String] {
 	override def lt(a: String, b: String): Boolean = a.toLowerCase > b.toLowerCase
 }
 
-class NodeBuilder[K, V, T <: ChildNode](val ordering: Ordering[K]) extends ChildrenBuilder[K, T] with RecordBuilder[K, V] {
+class NodeBuilder[K, V](val ordering: Ordering[K]) extends ChildrenBuilder[K, V] with RecordBuilder[K, V] {
 
 	private[NodeBuilder] class SimpleRecord[V](val data: V) extends Record[V]
 
-	private[NodeBuilder] class WithChildrenSeq[K, U <: ChildNode](val keys: Seq[K], val children: Seq[U]) extends WithChildren[K, U] with Node {
-		def this() = this(Seq[K](), Seq[U]())
+	private[NodeBuilder] class WithChildrenSeq[K, V](val keys: Seq[K], val children: Seq[ChildNode[V]]) extends WithChildren[K, V] with Node[V] {
+		def this() = this(Seq[K](), Seq[ChildNode[V]]())
 		override val size = keys.length
+		override def values: Seq[V] = {
+			val buf = new ListBuffer[V]
+			children foreach { child => buf.appendAll(child.values) }
+			buf
+		}
 	}
 
-	override def emptyNode() = newNode(Seq[K](), Seq[T]())
-	override def newNode(keys: Seq[K], children: Seq[T]) = new WithChildrenSeq[K, T](keys, children)
-	override def newNode(key: K, data: V): WithChildren[K, Record[V]] =
-		new WithChildrenSeq[K, Record[V]](Seq[K](key), Seq[Record[V]](newRecord(data)))
+	override def emptyNode() = newNode(Seq[K](), Seq[ChildNode[V]]())
+	override def newNode(keys: Seq[K], children: Seq[ChildNode[V]]) = new WithChildrenSeq(keys, children)
+	override def newNode(key: K, data: V) = new WithChildrenSeq(Seq(key), Seq(newRecord(data)))
 	override def newRecord(data: V): Record[V] = new SimpleRecord[V](data)
 
-	def insertValue(node: WithChildren[K, T], k: K, value: V) = insert(node, k, newRecord(value))
-	def updateValue(node: WithChildren[K, T], k: K, value: V) = update(node, k, newRecord(value))
+	def insertValue(node: WithChildren[K, V], k: K, value: V): NodeRef[K, V] = insert(node, k, newRecord(value))
+	def updateValue(node: WithChildren[K, V], k: K, value: V): NodeRef[K, V] = update(node, k, newRecord(value))
 
-	override def insert(node: WithChildren[K, T], k: K, child: T) = insert(node.keys, node.children, k, child)
-	override def delete(node: WithChildren[K, T], k: K) = delete(node.keys, node.children, k)
-	override def update(node: WithChildren[K, T], k: K, child: T) = update(node.keys, node.children, k, child)
+	override def insert(node: WithChildren[K, V], k: K, child: ChildNode[V]): NodeRef[K, V] = insert(node.keys, node.children, k, child)
+	override def delete(node: WithChildren[K, V], k: K): NodeRef[K, V] = delete(node.keys, node.children, k)
+	override def update(node: WithChildren[K, V], k: K, child: ChildNode[V]): NodeRef[K, V] = update(node.keys, node.children, k, child)
 
-	private def insert(keys: Seq[K], children: Seq[T], k: K, child: T): NodePointer[K, T] = {
+	private def insert(keys: Seq[K], children: Seq[ChildNode[V]], k: K, child: ChildNode[V]): NodeRef[K, V] = {
 		if (keys.isEmpty)
-			new NodePointer[K, T](newNode(Seq[K](k), Seq[T](child)), k, child, 0)
+			new NodeRef(newNode(Seq(k), Seq(child)), k, child, 0)
 		else {
-			val position = keys.indexWhere(ordering.lt(k, _))
-			new NodePointer[K, T](newNode(
+			val position = keys.indexWhere(ordering.gt(k, _)) + 1
+			new NodeRef(newNode(
 				keys.take(position) ++ Seq(k) ++ keys.slice(position, keys.size),
 				children.take(position) ++ Seq(child) ++ children.slice(position, children.size)),
 				k, child, position)
 		}
 	}
 
-	private def delete(keys: Seq[K], children: Seq[T], key: K): NodePointer[K, T] = {
+	private def delete(keys: Seq[K], children: Seq[ChildNode[V]], key: K): NodeRef[K, V] = {
 		val position = keys.indexOf(key)
 		if (position < 0) throw new IndexOutOfBoundsException("key " + key + " does not exist")
 
-		new NodePointer[K, T](newNode(
+		new NodeRef(newNode(
 			keys.take(position) ++ keys.slice(position + 1, children.size),
 			children.take(position) ++ children.slice(position + 1, children.size)),
 			key, children(position), position)
 	}
 
-	private def update(keys: Seq[K], children: Seq[T], k: K, child: T): NodePointer[K, T] = {
+	private def update(keys: Seq[K], children: Seq[ChildNode[V]], k: K, child: ChildNode[V]): NodeRef[K, V] = {
 		val position = keys.indexOf(k)
+
 		if (position < 0) throw new IndexOutOfBoundsException("key " + k + " does not exist")
 
-		new NodePointer[K, T](newNode(
+		new NodeRef(newNode(
 			keys,
 			children.take(position) ++ Seq(child) ++ children.slice(position + 1, children.size)),
 			k, children(position), position)
--- src/test/scala/net/itadinanta/rankr/NodeTestSuite.scala
+++ src/test/scala/net/itadinanta/rankr/NodeTestSuite.scala
@@ -5,7 +5,7 @@ import org.scalatest.FlatSpec
 import org.scalatest.ShouldMatchers
 
 class NodeTestSuite extends FlatSpec with ShouldMatchers {
-	val builder = new NodeBuilder[Int, String, Record[String]](IntAscending)
+	val builder = new NodeBuilder[Int, String](IntAscending)
 	"An empty node" should "contain no entries" in {
 		builder.emptyNode.size should be === 0
 	}
@@ -15,10 +15,20 @@ class NodeTestSuite extends FlatSpec with ShouldMatchers {
 		newNode.keys.length should be === 1
 	}
 
-	"A node after insertion" should "contain 1 entry" in {
+	"A node after value insertion" should "contain 1 entry" in {
 		val newNode = builder.newNode(1, "Value")
-		val addition = builder.insert(newNode, 2, "AnotherValue")
-		addition.node.asInstanceOf[WithChildren].keys.length should be === 2
+		val addition = builder.insertValue(newNode, 2, "AnotherValue")
+		addition.node.keys.length should be === 2
+		addition.node.keys should be === Seq(1,2)
+	}
+
+	"A node after value insertions" should "contain entries in order" in {
+		val one = builder.newNode(1, "One")
+		val two = builder.insertValue(one, 3, "Three")
+		val three = builder.insertValue(two.node, 2, "Two")
+		three.node.keys.length should be === 3
+		three.node.keys should be === Seq(1,2,3)
+		three.node.values should be === Seq("One", "Two", "Three")
 	}
 
 
