0140427c995df1b927bfbb8aba1d848b5f05c64e
Nicola Orru <nigu.orru@gmail.com>
2015-08-23 22:28:09 +0100

    Fixed Timeout usage after upgrade to Akka 2.4-M2

--- rnkr-support/src/main/scala/net/itadinanta/rnkr/core/arbiter/Arbiter.scala
+++ rnkr-support/src/main/scala/net/itadinanta/rnkr/core/arbiter/Arbiter.scala
@@ -69,7 +69,7 @@ class Gate[T](val target: T) extends Actor with Logging {
 
 trait GateWrapper[T] extends Arbiter[T] {
 	val gate: ActorRef
-	implicit val timeout = Timeout.intToTimeout(Int.MaxValue)
+	implicit val timeout = Timeout(Int.MaxValue seconds)
 	import Gate._
 
 	override def wqueue[R](f: T => R)(implicit t: ClassTag[R]): Future[R] = ask(gate, WriteRequest(f)).mapTo[R]
