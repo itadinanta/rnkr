991319fd42d47fab6473bd7c31e62b2e789b3ecf
Nicola Orru <nigu.orru@gmail.com>
2015-09-07 00:50:35 +0100

    Moved some rotten test code around, to be revived

---
+++ rnkr-app/src/test/scala/net/itadinanta/rnkr/main/test/TreeTest.scala
@@ -0,0 +1,25 @@
+package net.itadinanta.rnkr.main.test
+
+object TreeTest extends App {
+	// TODO: resurrect
+	//	val a = TreeArbiter.create(Tree.intStringTree())
+	//	val done = Promise[Boolean]
+	//
+	//	val b = new ListBuffer[Future[Option[Row[Int, String]]]]
+	//
+	//	for (i <- 1 to 1000) {
+	//		Future { b += a.put(i, i.toString()) map (Some(_)) }
+	//		for (j <- 1 to 1000) {
+	//			Future { b += a.get(i) }
+	//		}
+	//	}
+	//
+	//	sequence(b.toList) map { r =>
+	//		//		a.page(40, 10).map { r => println(r); done.success(true) }
+	//		a.get(990).map { r => println(r); done.success(true) }
+	//	}
+	//
+	//	done.future map {
+	//		case _ => system.shutdown
+	//	}
+}
